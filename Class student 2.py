877e9283b59963a06db9b0022aca904859aba9fc
punithsabitha245 <punith.sabitha@gmail.com>
2025-10-05 17:46:30 +0530

    Add files via upload

---
+++ Class student 2.py
@@ -0,0 +1,14 @@
+class student:
+    grade = 10
+    name = "penguin"
+
+    def introduction(self):
+        print("Hi,i am a student ")
+
+        def details(self):
+            print("my name is" , self.name)
+            print("I study in grade" , self.details)
+
+ob=student()
+ob.introduction()
+ob.details()
